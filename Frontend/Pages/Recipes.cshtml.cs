b72b9cc8cea6e3510178278948312a88dfea26a5
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-07-21 22:23:17 +0200

    ✅ ADD: RecipeDetail view

--- Frontend/Pages/Recipes.cshtml.cs
+++ Frontend/Pages/Recipes.cshtml.cs
@@ -11,10 +11,14 @@ public class RecipesModel : PageModel
 {
     public string Message { get; set; } = "Error";
     public List<Models.Recipe> Recipes { get; set; } = new();
+    public List<List<string>> CategoriesNames { get; set; } = new();
 
     public async Task OnGet()
     {
         Recipes = await Requests.ListRecipes();
+        // Map recipe.CategoriesIds to Names
+        List<Models.Category> categories = await Requests.ListCategories();
+        CategoriesNames = GetCategoriesNames(Recipes, categories);
         Message = "List handler fired";
     }
 
@@ -31,12 +35,34 @@ public class RecipesModel : PageModel
     public async Task OnPostUpdate(Guid id)
     {
         Recipes = await Requests.ListRecipes();
+        // Map recipe.CategoriesIds to Names
+        List<Models.Category> categories = await Requests.ListCategories();
+        CategoriesNames = GetCategoriesNames(Recipes, categories);
         Message = $"Update handler fired {id}";
     }
 
     public async Task OnPostDelete(Guid id)
     {
         Recipes = await Requests.ListRecipes();
+        // Map recipe.CategoriesIds to Names
+        List<Models.Category> categories = await Requests.ListCategories();
+        CategoriesNames = GetCategoriesNames(Recipes, categories);
         Message = $"Delete handler fired {id}";
+    }
+
+    private static List<List<string>> GetCategoriesNames(List<Models.Recipe> Recipes, List<Models.Category> categories)
+    {
+        Dictionary<Guid, string> catDict = new();
+        foreach (Models.Category cat in categories)
+            catDict.Add(cat.Id, cat.Name);
+        List<List<string>> catNames = new();
+        foreach(Models.Recipe recipe in Recipes)
+        {
+            List<string> names = new();
+            foreach (var id in recipe.CategoriesIds)
+                names.Add(catDict[id]);
+            catNames.Add(names);
+        }
+        return catNames;
     }
 }
