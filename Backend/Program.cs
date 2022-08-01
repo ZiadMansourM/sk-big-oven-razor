f393175bf67b684eb399f353cff0c82c1ddfafee
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-08-01 18:16:33 +0200

    ✅ EXPOSE: Backend under port 3000

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -7,7 +7,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(
+    options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    }
+);
 
 Main.Router(app);
 Recipe.Router(app);
